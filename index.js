286cbd0bb23f4a8486b5c6c929f8e27b12f45cc5
William Koller <wkoller25@gmail.com>
2020-03-07 13:42:12 -0300

    :wrench: adjusted server

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const customExpress = require("./config/custom-express");
-const app = customExpress();
-const port = 3003;
-app.listen(port, () => console.log(`server running: ${port}`));
+const conn = require("./database/connection");
+const tables = require("./database/tables");
+conn.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connect database");
+    tables.init(conn);
+    const app = customExpress();
+    const port = 3003;
+    app.listen(port, () => console.log(`server running: ${port}`));
+  }
+});
